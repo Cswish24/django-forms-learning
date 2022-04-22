b1eec7e1777eea287d91387da74a0ef99f0c3fef
Cameron <85236636+Cswish24@users.noreply.github.com>
2022-04-22 11:39:32 -0500

    add class views

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you)
+    path("thank-you", views.ThankYouView.as_view()),
+    path("reviews", views.ReviewsListView.as_view()), # updated to class views. need .asview for classes
+    path("detail/<int:pk>", views.ReviewDetailView.as_view())
 ]
